cff99cfdf4cca9b3a6686fbbd935c0affdce3f72
Damian Heaton <dh64784@gmail.com>
2018-05-18 00:06:56 +0100

    Fixed X-ORIGIN-HEADER verification

--- server.go
+++ server.go
@@ -34,7 +34,7 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-		return !(!strings.HasPrefix(r.Header.Get("Origin"), "https://"+config.SrvHost) && !strings.HasPrefix(r.Header.Get("Origin"), "http://"+config.SrvHost))
+		return (strings.HasPrefix(r.Header.Get("Origin"), "https://"+config.SrvHost) || strings.HasPrefix(r.Header.Get("Origin"), "http://"+config.SrvHost))
 	},
 }
 
