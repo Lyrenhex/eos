fa553aa0baa11130bec92b7396bef5a298bb38a6
Max Heaton <dh64784@gmail.com>
2020-09-16 19:15:15 +0100

    Remove headers

--- app/app.js
+++ app/app.js
@@ -1,22 +1,3 @@
-/*
-COPYRIGHT (C) Damian Heaton 2017 - 2020
-
-The storage of this file on a computer via means of browser 'caching', and the execution of such code by user browsers is permitted. This work cannot be duplicated, copied, distributed, or modified (neither privately nor publicly) without the express, written consent of Damian Heaton, whom can be contacted (at time of notice) at damian@lyrenhex.me. This software cannot be used for commercial purposes.
-
-THIS SOFTWARE IS DISTRIBUTED "AS IS", WITHOUT WARRANTY OF ANY FORM (EITHER EXPRESS OR IMPLIED), INCLUDING (BUT NOT LIMITED TO) ANY IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
-
-TL;DR:
-You may:
-- Store this file on a computer via a browser's cache, though not by any other means.
-- Store this file for purposes of search indexing.
-- Execute this code for the purpose of using the software.
-
-You may NOT:
-- Distribute, copy, replicate, or duplicate this copyrighted software without express, written permission from Damian Heaton.
-- Use this software, in any way, shape or form, for any commercial purpose(s).
-- Claim this software as your own, or attempt to imply affiliation with the software in any way that could be detrimental or unlawful, or to suggest that the software, or Damian Heaton, are represented by, or represent, yourself.
-*/
-
 // set the version number
 const VERSION = "3.0.0";
 
