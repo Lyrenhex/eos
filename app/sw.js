a0ed6b5d4a012dad293c7033aad5f83b71f50c70
Max Heaton <dh64784@gmail.com>
2020-09-19 17:27:43 +0100

    ...

--- app/sw.js
+++ app/sw.js
@@ -35,10 +35,10 @@ self.addEventListener("fetch", event => {
         console.log('Network request for ', event.request.url);
 
         response = fetch(event.request);
-        return caches.open(cache_name).then(cache => {
+        caches.open(cache_name).then(cache => {
           cache.put(event.request.url, response.clone());
-          return response;
         });
+        return response;
       }).catch(error => { })
   );
 });
