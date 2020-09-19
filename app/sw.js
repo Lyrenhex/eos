d786b0866cf4d6ec7b655d6c83dc99b007e13468
Max Heaton <dh64784@gmail.com>
2020-09-19 17:33:08 +0100

    oops, misplaced returns

--- app/sw.js
+++ app/sw.js
@@ -35,10 +35,10 @@ self.addEventListener("fetch", event => {
         console.log('Network request for ', event.request.url);
 
         return fetch(event.request).then(response => {
-          caches.open(cache_name).then(cache => {
+          return caches.open(cache_name).then(cache => {
             cache.put(event.request.url, response.clone());
+            return response;
           });
-          return response;
         });
       }).catch(error => { })
   );
